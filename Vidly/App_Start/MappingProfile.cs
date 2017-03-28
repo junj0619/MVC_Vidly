346da6a97d805ece722fc796a2f033dd19e5e452
Jun <Jun@JUN-PC>
2017-03-27 23:46:09 -0400

    Refactoring Mapping domian to dto , dto to domain. Remove row once delete confirm

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,15 +12,23 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c=>c.Id,opt=>opt.Ignore());
-            Mapper.CreateMap<CustomerDto, Customer>();
+            //Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+
             //Id is the key property for the Movie class, and a key property should not be changed.
             //That’s why we get this exception.To resolve this, you need to tell AutoMapper to ignore
             //Id during mapping of a MovieDto to Movie.
-            Mapper.CreateMap<Movie, MovieDto>().ForMember(m=>m.Id,opt=>opt.Ignore());
-            Mapper.CreateMap<MovieDto, Movie>();
+            //Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
 
-            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
+
+
 
         }
     }
